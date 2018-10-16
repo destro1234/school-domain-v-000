9d54b8a910416c4b850d1f8c04220bc6dcbcc76a
Elias Ventura <eliasventura23@gmail.com>
2018-10-16 11:35:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,19 @@
-require 'pry'
-
 class School
-  attr_reader :name, :roster
+
+  attr_accessor :roster
 
   def initialize(name)
-    @name= name
-    @roster= {}
+    @name = name
+    @roster = {}
   end
 
-  def add_student(name, grade)
-
-  if @roster[grade]
-  @roster[grade] << name
-else
-  @roster[grade] = []
-  add_student(name, grade)
+  def add_student(student, grade)
+    @roster[grade] = []
+      if @roster[grade]
+        @roster[grade]<< student
+    else
+      @roster[grade] << student
+      add_student(student, grade)
+    end
   end
-end
-
-def grade(grade)
-  return @roster[grade]
-end
-
-def sort
-  sorted_array = {}
-  roster.each do |key, value|
-    sorted_array[key] = value.sort
-  end
-
-  sorted_array
-end
 end
